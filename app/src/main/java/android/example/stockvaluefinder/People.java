e11fc92971169ce74357abcdfea68169595d26e1
ATM-Shifat <shifatatm@gmail.com>
2022-05-11 18:36:57 +0600

    People class added in main branch

---
+++ app/src/main/java/android/example/stockvaluefinder/People.java
@@ -0,0 +1,17 @@
+package android.example.stockvaluefinder;
+
+public class People {
+    private String name;
+    
+    public People(){
+        name = "NotSet";
+    }
+    public String getName() {
+        return name;
+    }
+    
+
+    public void setName(String oneName) {
+        this.name = oneName;
+    }
+}
